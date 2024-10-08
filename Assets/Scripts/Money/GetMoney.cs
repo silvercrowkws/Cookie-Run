2779f98a632647396f25531c6787da4a1a28f4e7
silvercrowkws <silvercrowkws@naver.com>
2024-10-08 15:39:42 +0900

    돈 먹을 때 소리 추가

--- Assets/Scripts/Money/GetMoney.cs
+++ Assets/Scripts/Money/GetMoney.cs
@@ -9,9 +9,23 @@ public class GetMoney : MonoBehaviour
     /// </summary>
     GameManager gameManager;
 
+    /// <summary>
+    /// 오디오 소스
+    /// </summary>
+    AudioSource audioSource;
+
+    /// <summary>
+    /// 스프라이트 렌더러
+    /// </summary>
+    SpriteRenderer spriteRenderer;
+
+
     private void Awake()
     {
         gameManager = GameManager.Instance;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,14 +36,40 @@ public class GetMoney : MonoBehaviour
             if (this.gameObject.CompareTag("Gold"))
             {
                 gameManager.Money += 5;
+
+                // 소리 재생
+                audioSource.Play();
             }
             else if(this.gameObject.CompareTag("Silver"))
             {
                 gameManager.Money += 1;
+
+                // 소리 재생
+                audioSource.Play();
             }
 
-            // 여기에 소리 추가하면 됨
-            Destroy(gameObject);
+            // 알파 값을 0으로 변경하여 사라지게 함
+            Color color = spriteRenderer.color;
+            color.a = 0; // 알파 값을 0으로 설정
+            spriteRenderer.color = color; // 색상 업데이트
+
+            // 오브젝트 삭제            
+            //Destroy(gameObject);
+            // 소리가 재생되는 동안 약간의 대기 시간을 두고 삭제
+            StartCoroutine(DestroyAfterSound());
         }
+    }
+
+    /// <summary>
+    /// 소리가 끝날 때까지 대기한 후 삭제시키는 코루틴
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DestroyAfterSound()
+    {
+        // 소리가 끝날 때까지 대기
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        // 게임 오브젝트 삭제
+        Destroy(gameObject);
     }
 }
